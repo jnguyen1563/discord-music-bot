2833494c8269c803fc55e2decd3a5cf78c2fb9c3
jgnuyen1563 <justinnguyen0821@gmail.com>
2021-03-06 01:13:57 -0800

    Added search feature

--- cogs/help.py
+++ cogs/help.py
@@ -31,7 +31,13 @@ class Help(commands.Cog, name='Help'):
             **Queue Controls**
             queue
             clearq
-            removeq <#>"""), inline=True)
+            removeq <#>
+            shuffle
+            **Playlist**
+            playlist <url>
+            **Search**
+            search <words>
+            playsearch <#>"""), inline=True)
         
         # Command descriptions
         help_embed.add_field(name='Description', value=(
@@ -47,9 +53,23 @@ class Help(commands.Cog, name='Help'):
             ----------------------------------------
             Shows the current list of songs in the queue
             Completely removes all songs from the queue
-            Removes song from the queue at a given position"""), inline=True)
+            Removes song from the queue at a given position
+            Shuffles the entire queue
+            ----------------------------------------
+            Adds playlist entries into queue
+            ----------------------------------------
+            Searches for the top 3 search results on Youtube
+            Plays the selected song from search results"""), inline=True)
 
         await ctx.send(embed=help_embed)
+
+    @commands.command()
+    async def banger(self, ctx, days=0):
+        await ctx.send(f'Days without a banger: **{days}**')
+
+    @commands.command()
+    async def sax(self, ctx):
+        await ctx.send("Big Band don't \"wadataaah\" to the \"shamaca\", 'cause that's a \"comacomalipachah\", dig?")
 
 def setup(bot):
     bot.add_cog(Help(bot))
--- cogs/music.py
+++ cogs/music.py
@@ -3,12 +3,15 @@ import discord
 from discord.ext import commands
 from discord.utils import get
 import youtube_dl
-import os
 
+import os
+import random
+from youtubesearchpython.__future__ import VideosSearch
 
 # Options for youtube-dl
 ydl_opts = {
     'format': 'bestaudio/best',
+    'noplaylist': 'True',
     'postprocessors': [{
         'key': 'FFmpegExtractAudio',
         'preferredcodec': 'mp3',
@@ -23,6 +26,7 @@ class Music(commands.Cog, name='Music'):
         self.voice = None
         self.loop = False
         self.queue = [] # Stored in (title,url) pairs
+        self.search_results = None
 
 ########## Bot Join/Leave ##########
 
@@ -38,7 +42,7 @@ class Music(commands.Cog, name='Music'):
             channel = ctx.message.author.voice.channel
         except AttributeError:
             print('No user in voice channel')
-            await ctx.send('No user in voice channel')
+            await ctx.send('*No user in voice channel*')
             return
 
         # Get the voice clients the bot is currently connected to
@@ -52,7 +56,7 @@ class Music(commands.Cog, name='Music'):
             self.voice = await channel.connect()
             print(f'Bot has connected to {channel}')
 
-        await ctx.send(f'Joined {channel}')
+        await ctx.send(f'*Joined* ***{channel}***')
 
     @commands.command(aliases=['l'])
     async def leave(self, ctx):
@@ -66,7 +70,7 @@ class Music(commands.Cog, name='Music'):
             channel = ctx.message.author.voice.channel
         except AttributeError:
             print('No user in voice channel')
-            await ctx.send('No user in voice channel')
+            await ctx.send('*No user in voice channel*')
             return
 
         # Get voice clients the bot is currently connected to
@@ -76,10 +80,10 @@ class Music(commands.Cog, name='Music'):
         if self.voice and self.voice.is_connected():
             await self.voice.disconnect()
             print(f'Bot has left {channel}')
-            await ctx.send(f'Left {channel}')
+            await ctx.send(f'*Left* ***{channel}***')
         else:
             print('Not in a voice channel')
-            await ctx.send('Not in a voice channel')
+            await ctx.send('*Not in a voice channel*')
 
 ########## Song Playing/Queue ##########
 
@@ -96,7 +100,7 @@ class Music(commands.Cog, name='Music'):
         # Add the video to the queue with (title, url) pairs
         self.queue.append((title, url))
         print(f'{title} added to queue (Position {len(self.queue)})')
-        await ctx.send(f'{title} added to queue (Position {len(self.queue)})')
+        await ctx.send(f'***{title}*** *added to queue* **(Position {len(self.queue)}**)')
 
 
     @commands.command(aliases=['q', 'show', 'queue'])
@@ -107,7 +111,7 @@ class Music(commands.Cog, name='Music'):
         # Checks to see if the queue is empty
         if not self.queue:
             print('Attempted to retrieve queue, but it is empty')
-            await ctx.send('The queue is currently empty')
+            await ctx.send('*The queue is currently empty*')
             return
 
         # Creates embed for song list
@@ -118,8 +122,15 @@ class Music(commands.Cog, name='Music'):
 
         # Creates the string containing queue information
         embed_message = ""
-        for position, song in enumerate(self.queue):
-            embed_message += f'{position+1}: {song[0]}\n'
+        if len(self.queue) > 10:
+            # If the queue is greater than 10 songs, limits to 10 and prints how many remaining
+            for index in range(10):
+                embed_message += f'{index+1}: {self.queue[index][0]}\n'
+            embed_message += f'*... and {len(self.queue) - 10} more*'
+        else:
+            # Prints every song to the queue
+            for position, song in enumerate(self.queue):
+                embed_message += f'{position+1}: {song[0]}\n'
 
         queue_embed.add_field(name='Song list', value=embed_message)
         # Sends the song queue as an embed
@@ -131,7 +142,7 @@ class Music(commands.Cog, name='Music'):
         Removes a song from the queue given its position
         '''
         index = int(index)
-        await ctx.send(f'Removed {self.queue[index-1][0]}')
+        await ctx.send(f'*Removed* ***{self.queue[index-1][0]}***')
         del self.queue[index-1]
 
     @commands.command(aliases=['clear'])
@@ -140,7 +151,15 @@ class Music(commands.Cog, name='Music'):
         Empties the song queue
         '''
         self.queue.clear()
-        await ctx.send('Queue has been cleared')
+        await ctx.send('*Queue has been cleared*')
+
+    @commands.command()
+    async def shuffle(self, ctx):
+        '''
+        Shuffles the entire queue
+        '''
+        random.shuffle(self.queue)
+        await ctx.send('*Shuffled the queue*')
 
     async def song_is_playing(self, ctx):
         '''
@@ -174,7 +193,7 @@ class Music(commands.Cog, name='Music'):
         else:
             # If moving on from a completed song, confirm finish
             if old_song:
-                asyncio.run_coroutine_threadsafe(ctx.send(f'Finished playing {old_song}'), self.bot.loop)
+                asyncio.run_coroutine_threadsafe(ctx.send(f'Finished playing *{old_song}*'), self.bot.loop)
 
             if self.queue:
                 # Remove the next song on the list from the queue and play it
@@ -204,7 +223,7 @@ class Music(commands.Cog, name='Music'):
             await self.remove_old_song(ctx)
 
         # Confirm audio setup
-        await ctx.send('Prepping the saxophone')
+        await ctx.send('***Prepping the sax***')
 
         # Download audio from youtube video with previously set ydl options
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
@@ -214,12 +233,12 @@ class Music(commands.Cog, name='Music'):
         # Search for downloaded audio file and rename it
         for file in os.listdir('./'):
             if file.endswith('.mp3'):
-                print(f'Renamed file: {file}\n')
+                print(f'Renamed file: {file}')
                 os.rename(file, 'song.mp3')
 
         # Print the current file being played
         display_name = song_info['title']
-        await ctx.send(f'Playing: {display_name}')
+        await ctx.send(f'*Playing:* ***{display_name}***')
         print('Playing\n')
 
         # Play the audio
@@ -227,6 +246,82 @@ class Music(commands.Cog, name='Music'):
         self.voice.source = discord.PCMVolumeTransformer(self.voice.source)
         self.voice.source.volume = 0.12
 
+########## Playlists ##########
+
+    @commands.command()
+    async def playlist(self, ctx, url:str):
+        '''
+        Takes in a playlist and adds all its entries to queue
+        '''
+
+        with youtube_dl.YoutubeDL({'quiet': 'True', 'ignoreerrors': 'True'}) as ydl:
+            print('Downloading playlist info')
+            await ctx.send('*Downloading playlist info*')
+            playlist_info = ydl.extract_info(url, download=False)
+
+        playlist_queue = []    
+        for song in playlist_info['entries']:
+            if not song:
+                continue
+            playlist_queue.append((song['title'], song['webpage_url']))
+
+        self.queue += playlist_queue
+
+        if not await self.song_is_playing(ctx):
+            self.play_next_song(ctx)
+
+########## Search ##########
+
+    @commands.command()
+    async def search(self, ctx, *, search_terms:str):
+        '''
+        Searches youtube for its first three results for search terms
+        '''
+        try:
+            # Search with search terms
+            video_search = VideosSearch(search_terms, limit=3)
+            video_results = await video_search.next()
+        except:
+            print('Could not complete search')
+            await ctx.send('Invalid search')
+            return
+
+        # Store search results
+        self.search_results = video_results['result']
+
+        # Initialize embed for search results
+        search_embed = discord.Embed(
+            title='Search Results',
+            description=f'for "{search_terms}"',
+            color=discord.Color.blurple()
+        )
+
+        for index, video in enumerate(video_results['result']):
+            search_embed.add_field(name=f'{index+1}. {video["title"]}', value=f'{video["channel"]["name"]} - {video["duration"]} - {video["viewCount"]["text"]}')
+
+        await ctx.send(embed=search_embed)
+
+    @commands.command()
+    async def playsearch(self, ctx, position):
+        '''
+        Picks a video from search results and adds it to queue 
+        '''
+        # Error if no search has been done yet
+        if not self.search_results:
+            await ctx.send('No search results found, use the >search command first')
+            return
+
+        # Choose the selected video from position
+        try:
+            selected_video = self.search_results[int(position)-1]
+            self.search_results.clear()
+        except:
+            await ctx.send('Invalid position number')
+            print('Tried to access search, but invalid index was given')
+            return
+        
+        # Play the selected video
+        await self.play(ctx, selected_video['link'])
 
 ########## Media controls ##########
 
@@ -242,10 +337,10 @@ class Music(commands.Cog, name='Music'):
         if self.voice and self.voice.is_playing():
             print('Music paused')
             self.voice.pause()
-            await ctx.send('Music paused')
+            await ctx.send('*Music paused*')
         else:
             print('Tried to pause, but no music playing')
-            await ctx.send('No music playing')
+            await ctx.send('*No music playing*')
 
     @commands.command(aliases=['r'])
     async def resume(self, ctx):
@@ -259,13 +354,13 @@ class Music(commands.Cog, name='Music'):
         if self.voice and self.voice.is_paused():
             print('Music resumed')
             self.voice.resume()
-            await ctx.send('Music resumed')
+            await ctx.send('*Music resumed*')
         elif self.voice and not self.voice.is_paused():
             print('Tried to resume, but music is already playing')
-            await ctx.send('Current song is already playing')
+            await ctx.send('*Current song is already playing*')
         else:
             print('Tried to resume, but no song is playing')
-            await ctx.send('No song to resume')
+            await ctx.send('*No song to resume*')
 
     @commands.command(aliases=['s', 'skip'])
     async def stop(self, ctx):
@@ -278,10 +373,10 @@ class Music(commands.Cog, name='Music'):
         if await self.song_is_playing(ctx):
             print('Song skipped')
             self.voice.stop()
-            await ctx.send('Song skipped')
+            await ctx.send('*Song skipped*')
         else:
             print('Tried to stop, but no music playing')
-            await ctx.send('No music playing')
+            await ctx.send('*No music playing*')
 
     @commands.command()
     async def loop(self, ctx):
@@ -289,10 +384,10 @@ class Music(commands.Cog, name='Music'):
         Toggles loop mode, which causes the bot to loop the currently playing song
         '''
         if self.loop:
-            await ctx.send('No longer looping')
+            await ctx.send('*No longer looping*')
             self.loop = False
         else:
-            await ctx.send('Looping current song')
+            await ctx.send('*Looping current song*')
             self.loop = True
 
 def setup(bot):
